f8492567b2a4b4d94b9cbff1d6bd7e440d4612e7
jaysinhzala <jaysinhzala1@gmail.com>
2026-04-24 15:34:09 -0400

    feat: add User entity with id field and annotations

---
+++ user-service/src/main/java/com/shopsphere/userservice/model/User.java
@@ -0,0 +1,19 @@
+package com.shopsphere.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
